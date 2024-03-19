0a9d32ecac20126310ee3ec6a48b781785ba4167
Jaid <6216144+Jaid@users.noreply.github.com>
2024-03-19 06:49:00 +0100

    Removed Webpack reference
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/OutputConfigPlugin.ts
+++
@@ -1,9 +0,0 @@
-import type {Compiler, RollupPluginInstance} from 'rollup'
-
-export class OutputConfigPlugin implements RollupPluginInstance {
-  apply(compiler: Compiler) {
-    compiler.hooks.beforeCompile.tap(`OutputConfigPlugin`, () => {
-      console.dir(compiler.options)
-    })
-  }
-}
