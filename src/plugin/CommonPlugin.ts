277dc507e40258bce64f2c38afe23467e8d358d6
Jaid <6216144+Jaid@users.noreply.github.com>
2024-03-20 06:24:00 +0100

    Removed unneeded properties
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/plugin/CommonPlugin.ts
+++ src/plugin/CommonPlugin.ts
@@ -1,11 +1,9 @@
 import type {ConfigBuilder, ConfigBuilderPlugin, Hooks} from '../ConfigBuilder.js'
-import type {PackageJson} from 'type-fest'
 
 export type Options = {}
 
 export class CommonPlugin implements ConfigBuilderPlugin {
   protected options: Options
-  protected pkg: PackageJson | undefined
   constructor(options: Partial<Options> = {}) {
     this.options = options
   }
--- src/plugin/PkgPlugin.ts
+++ src/plugin/PkgPlugin.ts
@@ -1,11 +1,9 @@
 import type {ConfigBuilder, ConfigBuilderPlugin, Hooks} from '../ConfigBuilder.js'
-import type {PackageJson} from 'type-fest'
 
 import publishimoPlugin from 'src/plugin/rollupPlugin/publishimo.js'
 
 export class PkgPlugin implements ConfigBuilderPlugin {
   options
-  pkg: PackageJson | undefined
   constructor(options?: {}) {
     this.options = {}
   }
