8b510a3f7a5ade40a7443ab4d8f4d3cf576d2415
Jaid <6216144+Jaid@users.noreply.github.com>
2024-03-20 06:48:14 +0100

    Simplified own Rollup config
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,7 +2,6 @@ import {buildConfig} from 'src/index.js'
 
 const rollupConfig = await buildConfig({
   outputFolder: `out/package/${process.env.NODE_ENV ?? `development`}`,
-  externals: true,
 })
 
 export default rollupConfig
