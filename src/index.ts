b8eae4609cdf707fe42381b779ebb1be98bc19a9
Jaid <6216144+Jaid@users.noreply.github.com>
2024-03-20 07:23:35 +0100

    Added more exports to index
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-export {buildConfig} from './buildConfig.js'
-export {ConfigBuilder, ConfigBuilder as default} from './ConfigBuilder.js'
+export {buildConfig} from 'src/buildConfig.js'
+export {ConfigBuilder, ConfigBuilder as default} from 'src/ConfigBuilder.js'
+
+export {CommonjsPlugin} from 'src/plugin/CommonjsPlugin.js'
+export {CommonPlugin} from 'src/plugin/CommonPlugin.js'
+export {ExternalsPlugin} from 'src/plugin/ExternalsPlugin.js'
+export {LoadAssetsPlugin} from 'src/plugin/LoadAssetsPlugin.js'
+export {MinifyPlugin} from 'src/plugin/MinifyPlugin.js'
+export {PkgPlugin} from 'src/plugin/PkgPlugin.js'
+export {TypescriptPlugin} from 'src/plugin/TypescriptPlugin.js'
+
+export {default as dtsBundleGenerator} from 'src/plugin/rollupPlugin/dts-bundle-generator.js'
+export {default as emitFile} from 'src/plugin/rollupPlugin/emit-file.js'
+export {default as publishimo} from 'src/plugin/rollupPlugin/publishimo.js'
