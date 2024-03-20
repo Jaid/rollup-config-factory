eea2092a16ca19b679822986fde61a6c7970da89
Jaid <6216144+Jaid@users.noreply.github.com>
2024-03-20 09:19:36 +0100

    Fixed building
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,7 +1,7 @@
 import {buildConfig} from 'src/index.js'
 
 const rollupConfig = await buildConfig({
-  outputFolder: `out/package/${process.env.NODE_ENV ?? `development`}`,
+  outputFolder: `dist/package/${process.env.npm_package_name ?? `default`}/${process.env.NODE_ENV ?? `development`}`,
 })
 
 export default rollupConfig
--- src/plugin/LoadAssetsPlugin.ts
+++ src/plugin/LoadAssetsPlugin.ts
@@ -4,19 +4,25 @@ import type {InputOptions} from 'zeug/types'
 import jsonPlugin from '@rollup/plugin-json'
 import yamlPlugin from '@rollup/plugin-yaml'
 
-type JsonPluginOptions = Parameters<typeof jsonPlugin['default']>[0]
-type YamlPluginOptions = Parameters<typeof yamlPlugin['default']>[0]
+/* eslint-disable @typescript-eslint/prefer-ts-expect-error */
+// @ts-ignore ts(2339)
+type JsonPluginOptions = NonNullable<Parameters<typeof jsonPlugin['default']>[0]>
+/* eslint-disable @typescript-eslint/prefer-ts-expect-error */
+// @ts-ignore ts(2339)
+type YamlPluginOptions = NonNullable<Parameters<typeof yamlPlugin['default']>[0]>
 
 export type Options = InputOptions<{
-  defaultsType: typeof defaultOptions
+  readonly defaultsType: typeof defaultOptions
+  readonly optionalOptions: {
+    yamlOptions: false | YamlPluginOptions
+  }
 }>
 
 const defaultOptions = {
   jsonOptions: {
     preferConst: true,
     indent: `  `,
-  } as false | JsonPluginOptions,
-  yamlOptions: undefined as false | YamlPluginOptions,
+  } as false | JsonPluginOptions | undefined,
 }
 
 export class LoadAssetsPlugin implements ConfigBuilderPlugin {
@@ -32,12 +38,12 @@ export class LoadAssetsPlugin implements ConfigBuilderPlugin {
       if (this.options.jsonOptions !== false) {
       /* eslint-disable @typescript-eslint/prefer-ts-expect-error */
       // @ts-ignore ts(2345)
-        builder.addRollupPlugin(jsonPlugin(this.options.jsonOptions))
+        builder.addRollupPlugin(jsonPlugin, this.options.jsonOptions)
       }
       if (this.options.yamlOptions !== false) {
       /* eslint-disable @typescript-eslint/prefer-ts-expect-error */
       // @ts-ignore ts(2345)
-        builder.addRollupPlugin(yamlPlugin(this.options.yamlOptions))
+        builder.addRollupPlugin(yamlPlugin, this.options.yamlOptions)
       }
     })
   }
