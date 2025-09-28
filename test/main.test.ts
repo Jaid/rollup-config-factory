c7f715e0a93e5001c82138f1660208dc514ee580
Jaid <6216144+Jaid@users.noreply.github.com>
2025-09-28 23:03:13 +0200

    Added higher timeout to tests
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- test/main.test.ts
+++ test/main.test.ts
@@ -2,11 +2,13 @@ import {test} from 'bun:test'
 
 import {runTest} from './lib/runTest.js'
 
-test(`minimal-development`, async () => runTest(`minimal-development`))
-test(`minimal-production`, async () => runTest(`minimal-production`))
-test(`basic-development`, async () => runTest(`basic-development`))
-test(`basic-production`, async () => runTest(`basic-production`))
-test(`with-dependency-development`, async () => runTest(`with-dependency-development`))
-test(`with-dependency-production`, async () => runTest(`with-dependency-production`))
-test(`minify-development`, async () => runTest(`minify-development`))
-test(`minify-production`, async () => runTest(`minify-production`))
+const timeout = 60_000
+
+test(`minimal-development`, async () => runTest(`minimal-development`), {timeout})
+test(`minimal-production`, async () => runTest(`minimal-production`), {timeout})
+test(`basic-development`, async () => runTest(`basic-development`), {timeout})
+test(`basic-production`, async () => runTest(`basic-production`), {timeout})
+test(`with-dependency-development`, async () => runTest(`with-dependency-development`), {timeout})
+test(`with-dependency-production`, async () => runTest(`with-dependency-production`), {timeout})
+test(`minify-development`, async () => runTest(`minify-development`), {timeout})
+test(`minify-production`, async () => runTest(`minify-production`), {timeout})
